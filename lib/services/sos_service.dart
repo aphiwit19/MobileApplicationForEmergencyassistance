5d06ea84c5664ff267d129b53dd63dcdc257ff58
Aphiwit <palaapiwit@gmail.com>
2025-04-18 16:19:15 +0700

    เพิ่มส่งตำแหน่งพร้อมไปกับข้อความ

--- lib/services/sos_service.dart
+++ lib/services/sos_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:ballauto/services/sms_service.dart';
 import 'package:ballauto/services/contact_service.dart';
 import 'package:ballauto/model/sos_history.dart';
@@ -21,11 +22,16 @@ class SosService {
       // ดึงหมายเลขโทรศัพท์
       final phoneNumbers = contacts.map((contact) => contact.phone).toList();
 
+      // ดึงตำแหน่งปัจจุบัน
+      final position = await _getCurrentPosition();
+      final locationMessage =
+          "ตำแหน่งปัจจุบัน: https://www.google.com/maps?q=${position.latitude},${position.longitude}";
+
       // สร้างข้อความ SOS
       final user = FirebaseAuth.instance.currentUser;
       final message = user != null
-          ? "แจ้งเหตุฉุกเฉินจาก ${user.email ?? 'ผู้ใช้'} กรุณาติดต่อกลับด่วน!"
-          : "แจ้งเหตุฉุกเฉิน กรุณาติดต่อกลับด่วน!";
+          ? "แจ้งเหตุฉุกเฉินจาก ${user.email ?? 'ผู้ใช้'}\n$locationMessage"
+          : "แจ้งเหตุฉุกเฉิน\n$locationMessage";
 
       // ส่ง SMS
       await _smsService.sendSms(phoneNumbers, message);
@@ -60,5 +66,33 @@ class SosService {
       }
       rethrow; // ส่งข้อผิดพลาดกลับไป
     }
+  }
+
+  Future<Position> _getCurrentPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // ตรวจสอบว่า Location Service เปิดอยู่หรือไม่
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception('Location services are disabled.');
+    }
+
+    // ตรวจสอบสิทธิ์การเข้าถึงตำแหน่ง
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // ดึงตำแหน่งปัจจุบัน
+    return await Geolocator.getCurrentPosition();
   }
 }
